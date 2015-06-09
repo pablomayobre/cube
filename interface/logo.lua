5ef07f1ba4df257ed6782558e02739bc2ba3749f
Pablo Mayobre <pablo.a.mayobre@gmail.com>
2015-06-09 17:32:16 -0300

    Fixed some issues with logo positioning
    I wont be commiting to this branch anymore, since it is really outdated,
    this commit is made in order to close this branch

--- interface/logo.lua
+++ interface/logo.lua
@@ -4,8 +4,8 @@ logo:load = function ()
 	self.icon	= love.graphics.newImage("icon.png")
 	self.cube	= love.graphics.newImage("cube.png")
 
-	self.w1,self.h1 = selftext:getDimensions()
-	self.w2,self.h2 = selfimage:getDimensions()
+	self.w1,self.h1 = self.cube:getDimensions()
+	self.w2,self.h2 = self.icon:getDimensions()
 end
 
 logo:draw = function (w,h)
@@ -13,11 +13,11 @@ logo:draw = function (w,h)
 	local self.w = w - (w/20)
 	--Weird way to determine which image shall be used and where should it be positioned
 	if self.w1 * self.h/self.h1 > self.w then
-		if self.h2 * self.w/self.h2 > self.h then
+		if self.h2 * self.w/self.w2 > self.h then
 			love.graphics.draw(self.icon,w/2,h/10,0,self.h/self.h2,nil,self.w2/2,self.h2/2)
 			return
 		else
-			love.graphics.draw(self.icon,w/2,h/10,0,self.w/self.h2,nil,self.w2/2,self.h2/2)
+			love.graphics.draw(self.icon,w/2,h/10,0,self.w/self.w2,nil,self.w2/2,self.h2/2)
 			return
 		end
 	else
